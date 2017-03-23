540cd5bd11ec0f03ad0d4e960330807ccb20ae25
Asif-newaz <max.asif24@gmail.com>
2017-03-23 05:34:21 -0700

    Add files via upload
    All about python dictionary

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+d = dict()
+d = dict(name = 'Asif',
+         phone = '01783',
+         email = 'max.asif24@gmail.com',
+         section = 'B',
+         Batch = '13',
+         Dept = 'SWE')
+print(d)
+print(d['name'])
+d[90] = 'good'
+print(d)
+print(d.keys())
+print(d.values())
+print(d.get('name'))
+print(d.get('email'))
+d['Location'] = 'DOL'
+print(d)
+print(d.items())
+d1 =  ('New DIct': 
